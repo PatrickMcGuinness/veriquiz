779215bb8eba288059cfad4dc94c375f1ea1f3d6
PatrickMcGuinness <patmcguinness@austin.rr.com>
2012-08-31 19:38:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Veriquiz::Application.config.secret_token = '7e0d5b4041f2e2629b6eb22f1d6db3246aab4a83cb7b6063ae2cea4acc847f1923dd4f06f584d32bd0d4b81aac10ef608c4843ce6f650dee7b58f5a02ec3d017'
